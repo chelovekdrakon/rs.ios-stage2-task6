b9207ec703f9d4637eb198de6430df289272381d
Morau Fiodar <MorauFiodar@yahoo.com>
2020-07-10 20:14:37 +0300

    feat: UIColor category with all app colors

---
+++ rs.ios-stage2-task6/categories/UIColor+CustomColor.h
@@ -0,0 +1,21 @@
+//
+//  UIColor+CustomColor.h
+//  rs.ios-stage2-task6
+//
+//  Created by Фёдор Морев on 7/10/20.
+//  Copyright © 2020 Фёдор Морев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (CustomColor)
+
++ (UIColor *)redColor; // <0xEE686A>
++ (UIColor *)blueColor; // <0x29C2D1>
++ (UIColor *)grayColor; // <0x707070>
++ (UIColor *)greenColor; // <0x34C1A1>
++ (UIColor *)blackColor; // <0x101010>
++ (UIColor *)yellowColor; // <0xF9CC78>
++ (UIColor *)yellowHighlightedColor; // <0xFDF4E3>
+
+@end
---
+++ rs.ios-stage2-task6/categories/UIColor+CustomColor.m
@@ -0,0 +1,69 @@
+//
+//  UIColor+CustomColor.m
+//  rs.ios-stage2-task6
+//
+//  Created by Фёдор Морев on 7/10/20.
+//  Copyright © 2020 Фёдор Морев. All rights reserved.
+//
+
+#import "UIColor+CustomColor.h"
+
+@implementation UIColor (CustomColor)
+
+// <0xEE686A>
++ (UIColor *)redColor {
+    return [UIColor colorWithRed:238.0f/255.0f
+                           green:104.0f/255.0f
+                            blue:106.0f/255.0f
+                           alpha:1.0f];
+}
+
+// <0x29C2D1>
++ (UIColor *)blueColor {
+    return [UIColor colorWithRed:41.0f/255.0f
+                           green:194.0f/255.0f
+                            blue:209.0f/255.0f
+                           alpha:1.0f];
+}
+
+// <0x707070>
++ (UIColor *)grayColor {
+    return [UIColor colorWithRed:112.0f/255.0f
+                           green:112.0f/255.0f
+                            blue:112.0f/255.0f
+                           alpha:1.0f];
+}
+
+// <0x34C1A1>
++ (UIColor *)greenColor {
+    return [UIColor colorWithRed:52.0f/255.0f
+                           green:193.0f/255.0f
+                            blue:161.0f/255.0f
+                           alpha:1.0f];
+}
+
+// <0x101010>
++ (UIColor *)blackColor {
+    return [UIColor colorWithRed:16.0f/255.0f
+                           green:16.0f/255.0f
+                            blue:16.0f/255.0f
+                           alpha:1.0f];
+}
+
+// <0xF9CC78>
++ (UIColor *)yellowColor {
+    return [UIColor colorWithRed:249.0f/255.0f
+                           green:204.0f/255.0f
+                            blue:120.0f/255.0f
+                           alpha:1.0f];
+}
+
+// <0xFDF4E3>
++ (UIColor *)yellowHighlightedColor {
+    return [UIColor colorWithRed:253.0f/255.0f
+                           green:244.0f/255.0f
+                            blue:227.0f/255.0f
+                           alpha:1.0f];
+}
+
+@end
